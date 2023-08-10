39651df384898e885851cb7c5837682ccfbf4b28
omolloicon <omollolevy307@gmail.com>
2023-08-10 09:21:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program thats outputs a line
+ * Return: 0 (output)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
