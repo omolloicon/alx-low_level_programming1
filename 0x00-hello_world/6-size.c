8eaee25253d86b8990aba28411f07a26886a3578
omolloicon <omollolevy307@gmail.com>
2023-08-10 10:24:30 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - C program that out outputs various computer types
+ * Return: 0 (output)
+ */
+int main(void)
+{
+	int a1;
+	char a2;
+	float a3;
+	long int a4;
+	long long int a5;
+
+printf("Size of an int: %lu byte(s) \n", sizeof(a1));
+printf("Size of a  char: %lu byte(s) \n", sizeof(a2));
+printf("Size of a float: %lu byte(s) \n", sizeof(a3));
+printf("Size of a long int: %lu byte(s) \n ", sizeof(a4));
+printf("Size of an long long int: %lu byte(s) \n ", sizeof(a1));
+return (0);
+}
