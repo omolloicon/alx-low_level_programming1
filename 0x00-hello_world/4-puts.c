7597d7dc52df5f8030fbbc834a76e00cde13e513
omolloicon <omollolevy307@gmail.com>
2023-08-10 08:52:05 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that outputs a given line
+ * Return: 0 (output)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
