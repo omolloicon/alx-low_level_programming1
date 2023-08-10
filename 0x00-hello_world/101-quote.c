03104c71483a756e725f4a4ead95ef51fcc66f8b
omolloicon <omollolevy307@gmail.com>
2023-08-10 11:41:41 -0700

    #include <stdio.h> #include <unistd.h>
    /**
    * main - Entry point
    *
    * Return: Always 1 (Success)
    */
    int main(void)
    {
    write(2,
    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
    return (1);

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - output in starndard error
- * Return: 1 (output)
- */
+* main - program to output a line
+*
+* Return: 1 (outline)
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
