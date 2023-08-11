0427539facfefbe42560c330bebcf549402479f5
omolloicon <omollolevy307@gmail.com>
2023-08-11 06:57:54 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - c program to print base 10 numbers
+ * the number starts from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
