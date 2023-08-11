3fe28b7fa3fc534b0167c2fd69eab3a75031ff3a
omolloicon <omollolevy307@gmail.com>
2023-08-11 06:47:33 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - c code to print nums from 0 to 9.
+ * in asending order
+ * Return: 0 (great)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		printf("%d", j);
+	}
+	putchar('\n');
+	return (0);
+}
