5a376378baa6a4c1012a4d9961c5a43cdd74e05b
omolloicon <omollolevy307@gmail.com>
2023-08-11 05:32:50 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - c code that prints a to z.
+ * the alphabets are 26
+ * Return: 0 (good)
+ */
+int main(void)
+{
+	char atoz[26] = "abcdefghijklmnopqrstuvwxyz";
+	int k;
+
+	for (k = 0; k < 26; k++)
+	{
+		putchar(atoz[k]);
+	}
+	putchar('\n');
+
+	return (0);
+
