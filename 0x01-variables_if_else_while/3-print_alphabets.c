55688d175d5b8654777993173832689bb5d1add5
omolloicon <omollolevy307@gmail.com>
2023-08-11 06:01:51 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - c program that prints alphabes
+ * prints (26) alphabes in small letters
+ * it prints the same in capital
+ * Return: 0 (Output)
+ */
+int main(void)
+{
+char atoz[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int l;
+
+	for (l = 0; l < 52; l + 1)
+	{
+		putchar(atoz[l]);
+	}
+	putchar('\n');
+	return (0);
+}
