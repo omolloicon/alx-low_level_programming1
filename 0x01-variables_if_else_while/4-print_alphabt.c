e1541cbe8c909e2359a0021c81b8241519c60e2e
omolloicon <omollolevy307@gmail.com>
2023-08-11 06:27:45 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - c program that prints alphabets excluding q and e * prints individual characters
+ * Return: 0
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 98; k < 124; k++)
+	{
+		if (k != 102 && k != 114)
+		{
+			putchar(k);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
