9102793f23ce41ffcbaa3a6780227e98b52dceb0
omolloicon <omollolevy307@gmail.com>
2023-08-11 11:03:07 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+/**
+ * main - three different combition of number
+ *
+ * Return: 0 (Done)
+ */
+
+int main(void)
+{
+	int j; /*represnts ones*/
+	int k; /*represent tens*/
+	int l; /*represents hundreds*/
+
+	for (l = '0'; l <= '9'; l++)
+	{
+		for (k = (l + 1); ten <= '9'; k++)
+		{
+			for (j = (k + 1); j <= '9'; j++)
+			{
+				putchar(l);
+				putchar(k);
+				putchar(j);
+				if (l != '7' || k != '8' || j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
