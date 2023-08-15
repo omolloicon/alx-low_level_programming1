c46a3f9a6a1165d0f7250b76716ee8412306b9f0
omolloicon <omollolevy307@gmail.com>
2023-08-15 09:24:01 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - main entry
+*
+* Return: 0
+*/
+int main(void)
+{
+long int e, f, h, all;
+
+f = 1;
+
+h = 2;
+
+for (e = 1; e <= 50; ++e)
+{
+if (f != 20365011074)
+{
+printf("%ld, ", f);
+}
+else
+{
+printf("%ld\n", f);
+}
+next = f + h;
+f = h;
+h = all;
+}
+
+return (0);
+}
