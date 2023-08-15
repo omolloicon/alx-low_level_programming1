6154a3f9669c3939e15cb4e6c7ce37ae746de075
omolloicon <omollolevy307@gmail.com>
2023-08-15 07:16:42 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - printng sign of num after check
+ * @n: checking valiabilty of num
+ * Return: 1 and prints + for > zero 0
+ * prints 0 for = zero
+ * -1 and prints - for < zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
