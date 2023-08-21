8ae06775ed2141e7293c74e583a2822f747fa1e8
omolloicon <omollolevy307@gmail.com>
2023-08-21 06:30:21 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - string display.
+ * @str: enter str.
+ * Return: nill.
+ */
+void _puts(char *str)
+{
+	int add = 0;
+
+	while (add >= 0)
+	{
+		if (str[add] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[add);
+		add++;
+	}
+}
