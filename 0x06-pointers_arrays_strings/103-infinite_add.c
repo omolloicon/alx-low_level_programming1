b49408942da66e039c01244169d2fca1d59b043c
omolloicon <omollolevy307@gmail.com>
2023-08-24 07:15:14 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,36 +12,36 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int sum = 0, s1, s2, k, l;
+	int sum = 0, s1, s2, k, l;
 
-for (s1 = 0; n1[s1]; s1++)
-;
-for (s2 = 0; n2[s2]; s2++)
-;
-if (s1 > size_r || s2 > size_r)
-return (0);
-s1--;
-s2--;
-size_r--;
-for (k = 0; k < size_r; k++, s1--, s2--)
-{
-if (s1 >= 0)
-sum += n1[s1] - '0';
-if (s2 >= 0)
-sum += n2[s2] - '0';
-if (s1 < 0 && s2 < 0 && sum == 0)
-break;
-r[k] = sum % 10 + '0';
-sum /= 10;
-}
-r[k] = '\0';
-if (s1 >= 0 || s2 >= 0 || sum)
-return (0);
-for (k--, l = 0; k > l; k--, l++)
-{
-add = r[k];
-r[k] = r[l];
-r[l] = add;
-}
-return (r);
+	for (s1 = 0; n1[s1]; s1++)
+		;
+	for (s2 = 0; n2[s2]; s2++)
+		;
+	if (s1 > size_r || s2 > size_r)
+		return (0);
+	s1--;
+	s2--;
+	size_r--;
+	for (k = 0; k < size_r; k++, s1--, s2--)
+	{
+		if (s1 >= 0)
+			sum += n1[s1] - '0';
+		if (s2 >= 0)
+			sum += n2[s2] - '0';
+		if (s1 < 0 && s2 < 0 && sum == 0)
+			break;
+		r[k] = sum % 10 + '0';
+		sum /= 10;
+	}
+	r[k] = '\0';
+	if (s1 >= 0 || s2 >= 0 || sum)
+		return (0);
+	for (k--, l = 0; k > l; k--, l++)
+	{
+		add = r[k];
+		r[k] = r[l];
+		r[l] = add;
+	}
+	return (r);
 }
