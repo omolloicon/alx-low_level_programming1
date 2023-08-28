5d43deadb39525bc2e9cde7dd98aca4dcc0759f1
omolloicon <omollolevy307@gmail.com>
2023-08-28 09:23:31 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,9 +11,11 @@
 
 void print_diagsums(int *a, int size)
 {
-	int y, x, add_a = 0, add_b = 0;
+	int y, x,  = 0, add_b = 0;
 
 	for (x = 0, y = size - 1; x < (size * size); x += size + 1, y += size - 1)
-		add_a += a[x], add_b += a[y];
+		add
+		       	+= a[x], add_b += a[y];
+
 	printf("%d, %d\n", add_a, add_b);
 }
