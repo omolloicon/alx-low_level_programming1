0fbba1830097427eac3bd6bafd38142e6acf9b67
omolloicon <omollolevy307@gmail.com>
2023-08-29 23:47:09 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _length - prints string length
+ * @s: the string under process
+ *
+ * Return: string size
+ */
+int _length(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+	return (1 + _length(s + 1));
+}
+
+/**
+ * _verify - string comparizon
+ * @s: string under process
+ * @len: value one
+ * @count: value n,to show its the biggest
+ *
+ * Return: done
+ */
+int _verify(char *s, int size, int tally)
+{
+	if (tally >= size)
+		return (1);
+	if (s[size] == s[tally])
+		return (_verify(s, size - 1, tally + 1));
+	return (0);
+}
+
+/**
+ * is_palindrome - identify palindrome
+ * @s: this is string under process
+ *
+ * Return: done
+ */
+int is_palindrome(char *s)
+{
+	int size = _length(s);
+	int tally = 0;
+
+	return (_verify(s, size - 1, tally));
+}
