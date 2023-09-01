2799610ca9eb8f33cd9f9450e31a0ac6f389cdaf
omolloicon <omollolevy307@gmail.com>
2023-09-01 09:39:15 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "ctype.h"
+
+/**
+ * main - adds two positive numbers
+ * @argc: check through
+ * @argv: where to store values
+ * Return: 0 (done)
+ */
+
+int main(int argc, char *argv[])
+{
+	int m, n, add;
+
+	add = 0;
+	if (argc > 1)
+	{
+		for (m = 1; < argc; m++)
+		{
+			for (n = 0; argv[m][n]; n++)
+			{
+				if (!isdigit(argv[m][n]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[m])
+		}
+		print("%d\n", add);
+	}
+	else
+	{
+		print("0\n");
+	}
+	return (0);
+
