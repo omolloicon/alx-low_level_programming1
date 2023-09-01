99cff87301860fd33e79161d70b1dfac5664fe70
omolloicon <omollolevy307@gmail.com>
2023-09-01 05:36:37 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - entry point
  * @argc: - counter
@@ -9,6 +8,6 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-		printf("%s\n", *argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
