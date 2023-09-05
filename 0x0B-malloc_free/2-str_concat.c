d5c7ed4b51e3bc4e9512a343a51ebeb567b3c0f5
omolloicon <omollolevy307@gmail.com>
2023-09-05 07:06:02 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,31 +11,33 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *g;
-	int ca = 0, cb = 0, m, n = 0, r = 0;
+	unsigned int m, n, z, max;
 
-	if (s1 != NULL)
-	{
-		while (s1[ca] != '\0')
-			cb++;
-	}
-	else if (s1 == NULL)
-		ca = 0;
-	if (s2 != NULL)
-	{
-		while (s2[cb] != '\0')
-			cb++;
-	}
-	else if (s2 == NULL)
-		cb = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	g = malloc((sizeof(char) * ca) + (sizeof(char) * cb) + 1);
+	for (m = 0; s1[m] != '\0'; m++)
+		;
+
+	for (n = 0; s2[n] != '\0'; n++)
+		;
+
+	g = malloc(sizeof(char) * (m + n + 1));
+
 	if (g == NULL)
+	{
+		free(g);
 		return (NULL);
+	}
 
-	for (m = 0; m < ca; m++, n++)
-		g[m] = s1[n];
-	for (; m < ca + cb; m++, r++)
-		g[m] = s2[r];
-	g[m] = '\0';
+	for (z = 0; z < m; z++)
+		g[z] = s1[z];
+
+	max = n;
+	for (n = 0; n <= max; k++, n++)
+		g[z] = s2[n];
+
 	return (g);
 }
