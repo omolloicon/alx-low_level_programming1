6975a92085374407399f71c64e8e56360a5f68fc
omolloicon <omollolevy307@gmail.com>
2023-09-05 04:49:08 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <main.h>
+#include <stdlib.h>
+
+/**
+ * create_array - proto type
+ * @size: mem capacity
+ * @c: data type
+ * Return: array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *m;
+	unsigned int n;
+
+	if (size == 0 || m == NULL)
+		return (NULL)
+
+	m = malloc(sizeof(char) * size);
+	for (n = 0; n < size; n++)
+		m[n] = c;
+
+	return (m);
+}
