4e118f25a3f7177e37326368cb269389791c1377
omolloicon <omollolevy307@gmail.com>
2023-09-12 06:37:20 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs memory.
+ * @d: dog's struct
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
