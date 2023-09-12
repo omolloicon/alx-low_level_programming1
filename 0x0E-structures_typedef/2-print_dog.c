30b99558c8543b2769079c6f3088a41faf7b96bb
omolloicon <omollolevy307@gmail.com>
2023-09-12 06:31:40 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,10 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Name: %s\n", d->name;
+			printf("Name: %s\n", d->name);
+		}
 
 		printf("Age: %f\n", d->age);
-		}
 
 		if (!(d->owner))
 		{
