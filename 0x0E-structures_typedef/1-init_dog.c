313a3046cac1c107d719e159af4b22af23656318
omolloicon <omollolevy307@gmail.com>
2023-09-12 04:58:41 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - struct dog.
+ * @d: dog's struct.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ * Return: done.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+		;
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
+}
