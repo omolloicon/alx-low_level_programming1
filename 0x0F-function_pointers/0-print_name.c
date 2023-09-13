c0b851f1fbc45de3b516a6b3e53aa1582b4abd33
omolloicon <omollolevy307@gmail.com>
2023-09-13 04:38:26 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ *print_name - function to display name
+ *@name: name
+ *@f: links to name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == 0 || f == o)
+		return;
+	(f)(name);
+}
