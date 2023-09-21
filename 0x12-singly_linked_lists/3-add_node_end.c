caf758cb27e9071b0c211f63b77c663348d74573
omolloicon <omollolevy307@gmail.com>
2023-09-21 09:07:26 -0700

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - size of strn
+ * @head: begging
+ * @str: string to use
+ *
+ * Return: sucess when done
+ **/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *start, *stop;
+	unsigned int cap = 0;
+	if (str == 0)
+		return (0);
+	start = malloc(sizeof(list_t));
+	if (start == 0)
+		return (0);
+	start->str = strdup(str);
+	if (start->str == 0)
+	{
+		free(start);
+		return (0);
+	}
+	while (str[cap])
+		cap++;
+	start->len = ;
+	start->next = 0;
+	if (*head == 0)
+	{
+		*head = start;
+		return (start);
+	}
+	stop = *head;
+	while (stop->next)
+		stop = stop->next;
+	stop->next = start;
+	return (start);
+}
