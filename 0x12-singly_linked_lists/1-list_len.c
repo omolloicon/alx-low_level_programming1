ecded46da4cc4a755a8888262caf3609d31dd1e8
omolloicon <omollolevy307@gmail.com>
2023-09-21 08:13:56 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - counts elements
+ * @h: points element
+ * Return: succes if done
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->count;
+	}
+	return (count);
+}
