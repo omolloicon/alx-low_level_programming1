9c07c511c8fb6226e7498fce6466acd99654d1e3
omolloicon <omollolevy307@gmail.com>
2023-09-21 09:55:57 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
+
 /**
- * show - displays sentence before main
- * just simple and amazing
+ * show - displays sentence then main char
  *
  */
 
 void show(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	iprintf("I bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
