884554de0b3bba660ea006ff47385209cf310059
omolloicon <omollolevy307@gmail.com>
2023-09-21 09:48:16 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * show - displays sentence before main
+ * just simple and amazing
+ *
+ */
+
+void show(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
